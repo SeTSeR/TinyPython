3e914a4667a585d916592bf403e92c66b5aca262
SeTSeR <sergey200018@bk.ru>
2015-06-02 10:37:08 +0300

    Finally fixed help

--- Cradle/main.py
+++ Cradle/main.py
@@ -2,13 +2,16 @@
 # -*- coding: UTF-8 -*-
 import input, scanner, parser, sys, errors
 
-if(len(sys.argv)<2):
-    errors.error('You must give exactly one argument!')
-if((sys.argv[1]=="--help") or (sys.argv[1]=='-h')):
+def printhelp():
     print('./main.py program\n')
     print('Interprets Tiny program from file program\n')
     print('./main.py --help(or -h)\n')
     print('Prints this help')
+
+if(len(sys.argv)<2):
+    printhelp()
+elif((sys.argv[1]=="--help") or (sys.argv[1]=='-h')):
+    printhelp()
 else:
     filename = sys.argv[1]
     input.init(filename)
